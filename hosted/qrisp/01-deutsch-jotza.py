6d8245ccc10283202ddfd3b5a74bf02b56e8c4f0
positr0mium <raphaelsdel@gmail.com>
2025-04-21 10:25:16 +0200

    applied some style improvements to qrisp programs

--- hosted/qrisp/01-deutsch-jotza.py
+++ hosted/qrisp/01-deutsch-jotza.py
@@ -1,5 +1,4 @@
 import qrisp
-
 def deutsch_jozsa(n, oracle):
     input_qubits = qrisp.QuantumVariable(n, name="input")
     aux_qubit = qrisp.QuantumVariable(1, name="aux")
@@ -10,14 +9,11 @@ def deutsch_jozsa(n, oracle):
     qrisp.h(input_qubits)
     qrisp.measure(input_qubits)
     return input_qubits.qs
-
 def constant_oracle(input_qubits, aux_qubit):
     pass
-
 def balanced_oracle(input_qubits, aux_qubit):
     for i in range(len(input_qubits)):
         qrisp.cx(input_qubits[i], aux_qubit)
-
 n = 3
 qc=deutsch_jozsa(n, constant_oracle)
 qc1=deutsch_jozsa(n, balanced_oracle)
